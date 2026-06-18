67a50555974890d4a0535648375e444c0721f283
Sami <samicrusader@boryeon.home.eravulgaris.jp>
2026-06-18 17:02:47 -0400

    openssl-1.0.2zo (stub, need source)
    If you have the source changes for this version, please contact me: hi@samicrusader.me

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -30,11 +30,11 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x1000227fL
+# define OPENSSL_VERSION_NUMBER  0x1000228fL
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2zn-fips  27 Jan 2026"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2zo-fips  ?? ??? 2026"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2zn  27 Jan 2026"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2zo  ?? ??? 2026"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
